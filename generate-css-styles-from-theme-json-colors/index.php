e4225f694a4b6784e02be34baff73282beb7cd85
Seth Rubenstein <srubenstein@pewresearch.org>
2023-03-27 09:52:58 -0400

    Initial commit with more comments to help explain utility.

---
+++ generate-css-styles-from-theme-json-colors/index.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Generates CSS styles from theme.json
+ *
+ * @return string
+ */
+
+function generate_css_styles_from_theme_json() {
+	if ( !function_exists('wp_get_global_settings') ) {
+		return new WP_Error('missing_function', 'wp_get_global_settings() is missing');
+	}
+
+	$colors = wp_get_global_settings();
+	$colors = $colors['color']['palette']['theme'];
+
+	ob_start();
+	foreach( $colors as $color ) {
+		$slug = $color['slug'];
+		$color = $color['color'];
+        // This class name follows the convention of the block's name and the color slug, in this example for text color "color".
+        // However, if you're using withColors and PanelColorSettings to generate your own color controls and attributes, this method is invaluable for generating all the css you'll need to support that, easily. You can make your classnames whatever you'd like; however, you should maintain the conventino, as it's the easiest way to know what the color is for. So if you're using a color attribute named "gridDividerColor", then your classname should be "has-grid-divider-color".
+		?>
+		.wp-block-my-block-name.has-<?php echo $slug; ?>-color .my-target {
+			color: <?php echo $color; ?> !important;
+            background-color: <?php echo $color; ?> !important;
+            border-color: <?php echo $color; ?> !important;
+		}
+		<?php
+	}
+	$styles = ob_get_clean();
+	return $styles;
+}
+
+/**
+ * Enqueues the CSS styles generated from theme.json on the site frontend and in the block and site editors (Gutenberg).
+ *
+ * @return void
+ */
+function enqueue_custom_fill_styles() {
+	$styles = generate_css_styles_from_theme_json();
+	if ( is_wp_error($styles) ) {
+		return;
+	}
+	wp_add_inline_style( 'my-block-style-handle', $styles );
+}
+add_action('wp_enqueue_scripts', 'enqueue_custom_fill_styles');
+add_action('enqueue_block_editor_assets', 'enqueue_custom_fill_styles');
